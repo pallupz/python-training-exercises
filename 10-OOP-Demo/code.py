48248c325fa66831d8fd152b90cceb710723400e
Antony Pallupetta <antony.pallupetta@experionglobal.com>
2022-04-12 10:01:30 +0530

    OOP methods

--- 10-OOP-Demo/code.py
+++ 10-OOP-Demo/code.py
@@ -1,27 +1,43 @@
 
 
 class Vehicle:
-    def __init__(self, color, num_tyres, mileage, tank_capacity):
+    def __init__(self, color, num_tyres, mileage, fuel_volume):
         self.color = color
         self.num_tyres = num_tyres
         self.mileage = mileage
-        self.tank_capacity = tank_capacity
+        self.fuel_volume = fuel_volume
 
-        self.range = mileage * tank_capacity
+        self.range = mileage * fuel_volume
+
+    def drive(self, distance):
+        fuel_used = distance / self.mileage
+        self.fuel_volume -= fuel_used
 
     @property
     def range_v2(self):
-        return self.mileage * self.tank_capacity
+        return self.mileage * self.fuel_volume
 
 
 car = Vehicle('black', 4, 15, 20)
 
+print('----------------')
 print(car.color)
 print(car.mileage)
 print(car.range)
 print(car.range_v2)
 
+print('----------------')
 car.mileage = 20
 print(car.mileage)
 print(car.range)
 print(car.range_v2)
+
+print('----------------')
+print(car.fuel_volume)
+print(car.range_v2)
+
+print('----------------')
+car.drive(20)
+print(car.fuel_volume)
+print(car.range_v2)
+
